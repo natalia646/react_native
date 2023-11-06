ee6d997b31f5f46818d99fa650423d0554af8f8c
natalia646 <nataxash46@gmail.com>
2023-11-06 20:07:50 +0200

    full desc

---
+++ screens/FullDescription.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+const FullDescription = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default FullDescription
